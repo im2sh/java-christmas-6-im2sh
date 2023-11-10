0591b43e26804aaa59e4950ca6a7852497ec53ab
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-11 04:08:52 +0900

    test: 주문 내역 출력 테스트 추가

---
+++ src/test/java/service/OrderServiceTest.java
@@ -0,0 +1,33 @@
+package service;
+
+import christmas.domain.FoodOrder;
+import christmas.response.OrderHistoryResponse;
+import christmas.service.OrderService;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    @Test
+    @DisplayName("주문 내역을 출력한다.")
+    public void 주문_내역_출력() throws Exception {
+        //given
+        Map<String,Integer> order = new HashMap<>();
+        order.put("해산물파스타",2);
+        order.put("초코케이크",1);
+        order.put("제로콜라",1);
+
+        //when
+        FoodOrder foodOrder = new FoodOrder(order);
+        OrderService orderService = new OrderService(foodOrder);
+        OrderHistoryResponse orderHistoryResponse = orderService.getOrderHistoryResponse();
+
+        //then
+        Assertions.assertEquals(foodOrder.getOrder(),orderHistoryResponse.getOrder());
+    }
+
+}
