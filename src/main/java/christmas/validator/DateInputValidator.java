1c7d4775713082fbe00f04eabaaad1e6e05d0488
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-11 01:16:48 +0900

    refactor(DateInputValidator): 메서드명 수정 및 메직넘버 상수로 변경

--- src/main/java/christmas/validator/DateInputValidator.java
+++ src/main/java/christmas/validator/DateInputValidator.java
@@ -1,33 +1,33 @@
 package christmas.validator;
 
 public class DateInputValidator {
+    private static final int START_DATE = 1;
+    private static final int END_DATE = 31;
     public DateInputValidator() {
     }
 
-    public int ValidateInputDate(String reservationDate) {
-        NotNullValidate(reservationDate);
-        NotValueValidate(reservationDate);
+    public int validateInputDate(String reservationDate) {
+        validateNotNull(reservationDate);
+        validateNotValue(reservationDate);
         int date = convertToInteger(reservationDate);
-        RangeValidate(date);
+        validateRange(date);
         return date;
     }
 
-    private void NotValueValidate(String reservationDate) {
+    private void validateNotValue(String reservationDate) {
         if (!reservationDate.matches("\\d+")) {
             throw new IllegalArgumentException("[ERROR] 입력 날짜는 숫자로만 입력되어야 합니다. 다시 입력해 주세요.");
         }
     }
 
-    private void NotNullValidate(String reservationDate) {
+    private void validateNotNull(String reservationDate) {
         if (reservationDate == null) {
             throw new IllegalArgumentException("[ERROR] 입력 값이 null 입니다. 다시 입력해 주세요.");
         }
     }
 
-    private void RangeValidate(int reservationDate) {
-        final int MIN_DATE = 1;
-        final int MAX_DATE = 31;
-        if (reservationDate < MIN_DATE || reservationDate > MAX_DATE) {
+    private void validateRange(int reservationDate) {
+        if (reservationDate < START_DATE || reservationDate > END_DATE) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않는 날짜입니다. 다시 입력해 주세요.");
         }
     }
