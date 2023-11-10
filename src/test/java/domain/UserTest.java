db348d3c239fd036630d34ac3745990296fab03b
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2023-11-11 01:21:21 +0900

    test: 입력날짜 테스트 리팩토링

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -2,6 +2,7 @@ package domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import christmas.domain.User;
 import christmas.validator.DateInputValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +12,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserTest {
     private DateInputValidator dateInputValidator;
+
     @BeforeEach
-    public void Init(){
+    public void Init() {
         dateInputValidator = new DateInputValidator();
     }
 
@@ -20,21 +22,24 @@ public class UserTest {
     @ValueSource(strings = {"a", "abc", "", "  ", "1일", "30일", "58일"})
     @DisplayName("입력 날짜가 숫자가 아닌 경우 예외가 발생한다.")
     public void 날짜_숫자_아닌_경우_테스트() throws Exception {
-        assertThatThrownBy(() -> dateInputValidator.ValidateInputDate("a")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new User(dateInputValidator.validateInputDate("a"))).isInstanceOf(
+                IllegalArgumentException.class);
     }
 
     @DisplayName("입력 날짜의 범위가 1 ~ 31일이 아닌 경우 예외가 발생한다.")
     @ValueSource(strings = {"32", "100", "0", "-1", "-100"})
     @ParameterizedTest
     public void 날짜_범위_테스트(String input) throws Exception {
-        assertThatThrownBy(() -> dateInputValidator.ValidateInputDate(input)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new User(dateInputValidator.validateInputDate(input))).isInstanceOf(
+                IllegalArgumentException.class);
     }
 
     @ParameterizedTest
     @NullSource
     @DisplayName("입력 날짜가 null인 경우 예외가 발생한다.")
     public void 날짜_문자열_null_테스트(String input) throws Exception {
-        assertThatThrownBy(() -> dateInputValidator.ValidateInputDate(input)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new User(dateInputValidator.validateInputDate(input))).isInstanceOf(
+                IllegalArgumentException.class);
     }
 
 }
